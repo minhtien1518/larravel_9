d64d74be4c7a8517cf40eb2c1f78872440a61be1
minhtien1518 <tienanh24102017@gmail.com>
2022-03-10 22:39:11 +0700

    lab1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,37 +8,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <button class="btn btn-primary">Xin chào cả nhà ạ</button>
+    
     <div class="container">
         <table class="table table-hover">
             <thead>
                 <th>Tên</th>
+                <th>Chiều Cao</th>
+                <th>Cân Nặng</th>
                 <th>Tuổi</th>
+                <th>Giới Tính</th>
                 <th>Lớp</th>
-                <th>MSV</th>
+                <th>ID</th>
                 <th>Ảnh đại diện</th>
             </thead>
             <tbody>
-             @foreach ($students as $students)
-             @if ($students['id'] ==1)
+             @foreach ($users as $users) 
+             @if ($users['id'] ==1)           
              <tr>
-                <th>{{$students['name']}}</th>
-                <th>{{$students['age']}}</th>
-                <th>{{$students['class']}}</th>
-                <th>{{$students['id']}}</th>
-                <th><img src="{{$students['avatar']}}" alt=""></th>
+                <th>{{$users['name']}}</th>
+                <th>{{$users['height']}}</th>
+                <th>{{$users['weight']}}</th>
+                <th>{{$users['age']}}</th>
+                <th>{{$users['gender']}}</th>
+                <th>{{$users['class']}}</th>
+                <th>{{$users['id']}}</th>
+                <th><img width="80px" src="{{$users['avatar']}}" alt=""></th>
             </tr>
-             @endif          
-            @endforeach
-            
+            @endif                   
+            @endforeach          
             </tbody>
-
         </table>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,37 +14,68 @@ use Illuminate\Http\Request;
 |
 */
 // thu muc view : resources/views/"welcome".blade.php
-Route::get('/', function () {
-    $students =[
-      [
-        'name' => 'chu',
-        'age' => '20',
-        'class' => 'web111',
-        'id' => '1',
-        'avatar' => ''
+// Route::get('/', function () {
+//     $students =[
+//       [
+//         'name' => 'chu',
+//         'age' => '20',
+//         'class' => 'web111',
+//         'id' => '1',
+//         'avatar' => ''
 
-      ],
-      [
-        'name' => 'chu123',
-        'age' => '22',
-        'class' => 'web222',
-        'id' => '2'
-      ],
-
-    ];
-    return view('welcome', ['students' => $students]);
-});
-
-Route::get('/users/{userId}/{userName?}', function(
-    Request $request,
-    $userId,
-    $userName ='profile'
-    ){
-    // dd($userId, $userName, $request->all());
-});
+//       ],
+//       [
+//         'name' => 'chu123',
+//         'age' => '22',
+//         'class' => 'web222',
+//         'id' => '2'
+//       ],
+//     ];
+//     return view('welcome', ['students' => $students]);
+// });
+// Route::get('/users/{userId}/{userName?}', function(
+//     Request $request,
+//     $userId,
+//     $userName ='profile'
+//     ){    
+// });
+// });
 // thu muc view : resources/views/auth/login.blade.php
 // Route::get('/login', function () {
 //     // dd('login view');
 //     $text = 'Xin chao ca nha';
 //     return view('auth.login')->with('text', $text);
 // });
+Route::get('users/{usersId?}/{usersName?}/{usersClass?}/{usersInfo?}',function (
+  Request $request,
+  $usersId ='1',
+  $usersName = 'tien11',
+  $usersClass = 'web16201',
+  $usersInfo = 'title'
+  )
+{
+      $users = 
+      [
+          [
+              'name' => 'Minh Tiến',
+              'height' => '1m72',
+              'weight' => '66',
+              'gender' => 'Nam',
+              'age' => '22',
+              'class'=> 'we16201', 
+              'id' => '1',
+              'avatar' =>'https://media-cdn-v2.laodong.vn/storage/newsportal/2019/6/10/738354/595097-01.jpg'
+          ],
+      ];
+return view('welcome',
+[
+  'users' => $users,
+  
+  'usersId' => $usersId,
+  'usersName' => $usersName,
+  'usersClass'  => $usersClass,
+  'usersInfo' => $usersInfo,
+  'request' => $request,
+
+  ]);
+});
